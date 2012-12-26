64b3430ce96e52814f768f6befc6e45562526bb4
Vitaut Bajaryn <vitaut.bayaryn@gmail.com>
2012-12-26 15:05:41 +0300

    Rename api_secret -> api_access_token

--- lib/vk_api/session.rb
+++ lib/vk_api/session.rb
@@ -8,7 +8,7 @@
 #
 # Библиотека VkApi имеет один класс - +::VK:Session+. После создания экземпляра сессии
 # вы можете вызывать методы ВКонтакте как будто это методы сессии, например:
-#   session = ::VkApi::Session.new app_id, api_secret
+#   session = ::VkApi::Session.new app_id, api_access_token
 #   session.friends.get :uid => 12
 # Такой вызов вернёт вам массив хэшей в виде:
 #   # => [{'uid' => '123'}, {:uid => '321'}]
@@ -29,13 +29,13 @@ module VkApi
     VK_API_URL = 'https://api.vk.com'
     VK_OBJECTS = %w(users friends photos wall audio video places secure language notes pages offers
       questions messages newsfeed status polls subscriptions likes)
-    attr_accessor :app_id, :api_secret
+    attr_accessor :app_id, :api_access_token
 
     # Конструктор. Получает следующие аргументы:
     # * app_id: ID приложения ВКонтакте.
-    # * api_secret: Ключ приложения со страницы настроек
-    def initialize app_id, api_secret, method_prefix = nil
-      @app_id, @api_secret, @prefix = app_id, api_secret, method_prefix
+    # * api_access_token: access_token, полученный из https://oauth.vk.com/authorize?client_id=##APP_ID##&redirect_uri=http://api.vk.com/blank.html&scope=##PERMISSIONS##&display=page&response_type=token
+    def initialize app_id, api_access_token, method_prefix = nil
+      @app_id, @api_access_token, @prefix = app_id, api_access_token, method_prefix
     end
 
     # Post request using https
@@ -59,7 +59,7 @@ module VkApi
     def call(method, params = {})
       method = method.to_s.camelize(:lower)
       method = @prefix ? "#{@prefix}.#{method}" : method
-      params[:access_token] = api_secret
+      params[:access_token] = api_access_token
 
       # http://vk.com/developers.php?oid=-1&p=%D0%92%D1%8B%D0%BF%D0%BE%D0%BB%D0%BD%D0%B5%D0%BD%D0%B8%D0%B5_%D0%B7%D0%B0%D0%BF%D1%80%D0%BE%D1%81%D0%BE%D0%B2_%D0%BA_API
       # now VK requires the following url: https://api.vk.com/method/METHOD_NAME
@@ -77,7 +77,7 @@ module VkApi
       ::VkApi::Session.class_eval do
         define_method method do
           if (! var = instance_variable_get("@#{method}"))
-            instance_variable_set("@#{method}", var = ::VkApi::Session.new(app_id, api_secret, method))
+            instance_variable_set("@#{method}", var = ::VkApi::Session.new(app_id, api_access_token, method))
           end
           var
         end
